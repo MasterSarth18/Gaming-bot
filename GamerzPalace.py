4a5d4925a8ae75907117a3e504b25f7ef1f2dfb0
MasterSarth18 <ghasesarth@gmail.com>
2025-07-29 15:00:46 +0530

    Update GamerzPalace.py

--- GamerzPalace.py
+++ GamerzPalace.py
@@ -1,1 +1,21 @@
 
+from openai import OpenAI
+
+client=OpenAI(
+    api_key="your_groq_api_key",
+    base_url="https://api.groq.com/openai/v1"
+)
+
+while True:
+    user_input= input("you: ")
+    if user_input =="quit":
+        print("bot: Goodbye")
+        break
+    response = client.chat.completions.create(
+        model="llama3-70b-8192",
+        messages=[
+            {"role": "system", "content": "you are a gaming bot, provide information only related to gaming"},
+            {"role": "user", "content": user_input}
+        ]
+    )
+    print("Bot: ", response.choices[0].message.content)
